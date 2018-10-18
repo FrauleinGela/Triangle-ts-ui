b2919c6f5213acd6e8a457e06ce8c23be8ece9e9
Ngela <ngela@Ngela-Pat-Yu.zyxel.com>
2018-10-19 00:09:16 +0200

    shared/shared.module-Update module to import new providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomFormValidator } from './helpers';
+import { TriangleFormService } from "./services";
 
 @NgModule({
   imports: [CommonModule],
-  exports: [CommonModule]
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [
+    CustomFormValidator,
+    TriangleFormService
+  ]
 })
 export class SharedModule { }
